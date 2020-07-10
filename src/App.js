792f4365087db47572a3afed9f9253e8001cfb31
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-10 11:06:37 -0700

    created routes for three pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
 function App() {
   return (
     <div className="App">
-      <h1>Test App</h1>
-
+      <Router>
+        <Route exact path="/" component={home} />
+        <Route exact path="/login" component={login} />
+        <Route exact path="/signup" component={signup} />
+      </Router>
     </div>
   );
 }
