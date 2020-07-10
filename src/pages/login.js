410d8dec07fd97eccffedd12e109752768babbf5
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-10 14:48:53 -0700

    created Login component

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
-
+import { TextField } from '@material-ui/core';
+import withStyles from '@material-ui/core/styles/withStyles'
+import PropTypes from 'prop-types'
+//MUI
+import Grid from '@material-ui/core/Grid'
+const styles = {
+  form: {
+    textAlign: 'center'
+  }
+}
 class Login extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <h1>Login Page</h1>
-      </div>
+      <Grid container className={classes.form}>
+        <Grid item sm></Grid>
+        <Grid item sm>Login</Grid>
+        <Grid item sm></Grid>
+      </Grid>
     )
   }
 }
 
-export default Login;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(Login);
