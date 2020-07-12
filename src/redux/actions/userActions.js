5fb5ceacea84a57454da3ce3e7765d06e00ce03e
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-12 16:13:29 -0700

    added editUserDetails to userActions

--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -69,3 +69,12 @@ export const uploadImage = (formData) => (dispatch) => {
     })
     .catch(err => console.log(err))
 }
+
+export const editUserDetails = (userDetail) => (dispatch) => {
+  dispatch({ type: LOADING_USER });
+  axios.post('/user', userDetail)
+    .then(() => {
+      dispatch(getUserData())
+    })
+    .catch(err => { console.log(err) });
+}
