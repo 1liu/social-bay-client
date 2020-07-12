f654c4444e8c9f44fdfe39e00259ec55d2287efa
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-12 11:49:00 -0700

    added user reducer types

---
+++ src/redux/types.js
@@ -0,0 +1,5 @@
+// User reducer types
+export const SET_AUTHTICATED = 'SET_AUTHTICATED'
+export const SET_UNAUTHTICATED = 'SET_UNAUTHTICATED'
+export const SET_USER = 'SET_USER'
+export const LOADING_USER = 'LOADING_USER'
