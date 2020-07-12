5e89115523fe9938214a5a92671f38fd8c9335ef
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-12 11:50:10 -0700

    added ui reducer types

--- src/redux/types.js
+++ src/redux/types.js
@@ -3,3 +3,10 @@ export const SET_AUTHTICATED = 'SET_AUTHTICATED'
 export const SET_UNAUTHTICATED = 'SET_UNAUTHTICATED'
 export const SET_USER = 'SET_USER'
 export const LOADING_USER = 'LOADING_USER'
+
+// ui reducer types
+export const SET_ERRORS = 'SET_ERRORS'
+export const LOADING_UI = 'LOADING_UI'
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
+
+// data druder types
