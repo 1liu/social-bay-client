41e41bf3603951d6199652d14a385d91f86f95de
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-14 14:10:09 -0700

    removed Login and Signup from Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import MyButton from '../lib/MyButton'
 import CreatePost from './CreatePost'
+import Notification from './Notification'
 //redux
 import { connect } from 'react-redux'
 //MUI
@@ -10,7 +11,7 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import Button from '@material-ui/core/Button'
 import HomeIcon from '@material-ui/icons/Home'
-import NotificationIcon from '@material-ui/icons/Notifications'
+
 class Navbar extends Component {
   render() {
     const { authenticated } = this.props;
@@ -28,15 +29,16 @@ class Navbar extends Component {
                   <HomeIcon color="primary" />
                 </MyButton>
               </Link>
-              <MyButton tip="Notification">
-                <NotificationIcon color="primary" />
-              </MyButton>
+              <Notification />
             </Fragment>
           ) : (
               <Fragment>
-                <Button color="inherit" component={Link} to='/'>Home</Button>
+                <Link to='/'>
+                  <HomeIcon color="primary" />
+                </Link>
+                {/* <Button color="inherit" component={Link} to='/'>Home</Button>
                 <Button color="inherit" component={Link} to='/signup'>Signup</Button>
-                <Button color="inherit" component={Link} to='/login'>Login</Button>
+                <Button color="inherit" component={Link} to='/login'>Login</Button> */}
               </Fragment>
             )
           }
