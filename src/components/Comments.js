638dee5d13f3f8859a0bf8d0b14cc5b1701fa0e0
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-14 11:48:10 -0700

    cleared unused stuff

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -7,8 +7,6 @@ import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import withStyles from '@material-ui/core/styles/withStyles';
 
-//redux
-import { connect } from 'react-redux'
 
 const styles = theme => ({
   ...theme.global,
